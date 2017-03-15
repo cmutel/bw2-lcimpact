4be924ef78bf9792caabacc434d8468908965900
Chris Mutel <cmutel@gmail.com>
2017-03-15 12:29:36 +0100

    First commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+LC IMPACT regionalized LCIA method for Brightway2
+=================================================
+
+This package provides the impact of the LC IMPACT reigonalized LCIA method in `brightway2 <https://brightwaylca.org>`__ and `brightway2-regional <https://bitbucket.org/cmutel/brightway2-regional>`__.
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('bw2_lcimpact'):
+    # Ignore dirnames that start with '.'
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+
+setup(
+    name='bw2-lcimpact',
+    version="0.1",
+    packages=packages,
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE.txt').read(),
+    install_requires=[
+        "brightway2",
+    ],
+    url="https://github.com/cmutel/bw2-lcimpact",
+    long_description=open('README.rst').read(),
+    description=('LC IMPACT regionalized LCIA method for Brightway2'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
